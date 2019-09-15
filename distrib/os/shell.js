9b4d39e365429e91f6f4c32bdb8a004d1615bf70
Jenna Daly <jennadaly@148-100-137-14.FoxNet.marist.edu>
2019-09-15 16:38:25 -0400

    Fixed load by converting input to lower case for validation

--- distrib/os/shell.js
+++ distrib/os/shell.js
@@ -235,7 +235,7 @@ var TSOS;
             var allowedChars = [' ', 'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
             var isValid = false;
             for (var i = 0; i < validateText.length; i++) {
-                if (allowedChars.indexOf(validateText[i]) > -1) {
+                if (allowedChars.indexOf(validateText[i].toLowerCase()) > -1) {
                     isValid = true;
                     //_StdOut.putText("Input is allowed.");
                 }
--- source/os/shell.ts
+++ source/os/shell.ts
@@ -302,7 +302,7 @@ module TSOS {
             var allowedChars = [' ', 'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
             let isValid: boolean = false;
             for(let i=0; i< validateText.length; i++) {
-                if(allowedChars.indexOf(validateText[i]) > -1 ) {
+                if(allowedChars.indexOf(validateText[i].toLowerCase()) > -1 ) {
                     isValid = true;
                     //_StdOut.putText("Input is allowed.");
                 }
